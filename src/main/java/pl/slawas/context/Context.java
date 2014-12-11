1aeca9fac8a34bd5d75d9583b72e94526a110548
Karol Kowalczyk <kkowalczyk@ibpm.pro>
2014-12-11 23:41:37 +0100

    MERC-116 Dodanie kontekstu wywołania aplikacji

---
+++ src/main/java/pl/slawas/context/Context.java
@@ -0,0 +1,188 @@
+/**
+ * 
+ */
+package pl.slawas.context;
+
+/**
+ * Context.java podstawowe metody obsługi wejścia do metod
+ * 
+ * @author Karol Kowalczyk
+ */
+public class Context {
+
+	/**
+	 * Domyślny konstruktor
+	 */
+	public Context() {
+	}
+
+	/**
+	 * Parametry kontekstu
+	 */
+	private String userName;
+	private String appName;
+	private String appVersion;
+	private int queryTimeout;
+	private String locale;
+	private String timeZone;
+
+	/**
+	 * Tekst do logowania parametrów
+	 */
+	@Override
+	public String toString() {
+		return "Context[" + userName + ' ' + appName + ' ' + appVersion + ' '
+				+ queryTimeout + ' ' + locale + ' ' + timeZone + ']';
+	}
+
+	/**
+	 * Konstruktor do szybkiego ustawienie parametrów wg kolejności za pomocą
+	 * listy stringów
+	 */
+	public Context(final String... properties) {
+		this();
+		if (properties != null) {
+			int i = 0;
+			String property;
+			while (i < properties.length) {
+				property = properties[i++];
+				switch (i) {
+				case 1:
+					userName = property;
+					break;
+				case 2:
+					appName = property;
+					break;
+				case 3:
+					appVersion = property;
+					break;
+				case 4:
+					queryTimeout = Integer.parseInt(property);
+					break;
+				case 5:
+					locale = property;
+					break;
+				case 6:
+					timeZone = property;
+					break;
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Nazwa użytkownika usługi
+	 * 
+	 * @return Nazwa użytkownika usługi
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            Nazwa użytkownika usługi
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * Parametry językowe użytkownika w formacie 'język_terytorium' np.
+	 * {@code pl_PL}
+	 * 
+	 * @return Parametry językowe użytkownika
+	 */
+	public String getLocale() {
+		return locale;
+	}
+
+	/**
+	 * @param locale
+	 *            definicja lokalizacji użytkownika w postaci string'a np.
+	 *            {@code 'pl_PL'}
+	 */
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+
+	/**
+	 * Strefa czasowa użytkownika
+	 * 
+	 * @return Strefa czasowa użytkownika
+	 */
+	public String getTimeZone() {
+		return timeZone;
+	}
+
+	/**
+	 * Ustawienie strefy czasowej użytkownika.
+	 * 
+	 * @param timeZone
+	 *            definicja strefy czasowej użytkownika w postaci string'a np.
+	 *            {@code 'Europe/Warsaw'}. Zobacz
+	 *            {@link TimeZone#getAvailableIDs()}.
+	 */
+	public void setTimeZone(String timeZone) {
+		this.timeZone = timeZone;
+	}
+
+	/**
+	 * Nazwa aplikacji użytkownika usługi
+	 * 
+	 * @return Nazwa aplikacji użytkownika usługi
+	 */
+	public String getAppName() {
+		return appName;
+	}
+
+	/**
+	 * Ustawienie nazwy aplikacji użytkownika usługi.
+	 * 
+	 * @param appName
+	 *            Nazwa aplikacji użytkownika usługi
+	 */
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	/**
+	 * Wersja aplikacji użytkownika usługi
+	 * 
+	 * @return Wersja aplikacji użytkownika usługi
+	 */
+	public String getAppVersion() {
+		return appVersion;
+	}
+
+	/**
+	 * Ustawienie wersji aplikacji użytkownika usługi.
+	 * 
+	 * @param appVersion
+	 *            Wersja aplikacji użytkownika usługi
+	 */
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+
+	/**
+	 * Timeout zapytania w bazie danych. Wartość 0 oznacza brak ustawienia.
+	 * 
+	 * @return Timeout zapytania w bazie danych
+	 */
+	public int getQueryTimeout() {
+		return queryTimeout;
+	}
+
+	/**
+	 * Ustawienie timeout zapytania w bazie danych. Wartość 0 oznacza brak
+	 * ustawienia.
+	 * 
+	 * @param queryTimeout
+	 *            Timeout zapytania w bazie danych
+	 */
+	public void setQueryTimeout(int queryTimeout) {
+		this.queryTimeout = queryTimeout;
+	}
+}
